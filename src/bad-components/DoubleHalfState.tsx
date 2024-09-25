33799f9da66f87b50b74810320a951c9cffe83e6
SUCHI <svp@udel.edu>
2024-09-25 17:49:16 -0400

    components

--- src/bad-components/DoubleHalfState.tsx
+++ src/bad-components/DoubleHalfState.tsx
@@ -1,3 +1,3 @@
-import { useState } from "react";
-
-export const [dhValue, setDhValue] = useState<number>(10);
+// import { useState } from "react";
+// export const [dhValue, setDhValue] = useState<number>(10);
+export {};
